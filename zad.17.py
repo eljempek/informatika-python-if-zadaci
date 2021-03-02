4923ba52cd193e71c025bfd31b14b70362df626c
eljempek <elenka.strakova@gmail.com>
2021-03-02 10:24:56 +0100

    zadatak 17 urobeni na hodine

---
+++ zad.17.py
@@ -0,0 +1,9 @@
+# Da li vazi a>b>c za data tri cela broja
+a = int(input("Vneste a "))
+b = int(input("Vneste b "))
+c = int(input("Vneste c "))
+if (a > b and b > c):
+    print("Plati")
+else:
+    print("Neplati")
+ 
