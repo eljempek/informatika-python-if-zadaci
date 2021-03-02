18ed2aa594b6ad77f912e72dba35a7281b44ed7f
f4pl0 <straka.filip@gmail.com>
2021-03-02 10:15:23 +0100

    Add zad17.py

---
+++ zad17.py
@@ -0,0 +1,12 @@
+# Input
+a = int(input("Vneste a: "))
+b = int(input("Vneste b: "))
+c = int(input("Vneste c: "))
+
+# Check
+if (a > b and b > c):
+    # a > b > c
+    print("Plati a>b>c")
+else:
+    # Neplati
+    print("Neplati a>b>c")
